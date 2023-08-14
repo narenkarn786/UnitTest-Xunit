1ba44f0512003bee43ecd91c1d6e355e5d56912d
narenkarn786@gmail.com <narenkarn786@gmail.com>
2023-08-14 10:59:16 +0545

    Add project files.

---
+++ ConsoleApp/Ping/NetworkUtility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.DNS;
+
+namespace ConsoleApp.Ping
+{
+    public class NetworkUtility
+    {
+        private readonly IDNS _dNS;
+
+        public NetworkUtility(IDNS dNS)
+        {
+            _dNS = dNS;
+        }
+        public string SendPing()
+        {
+            var dnssuccess = _dNS.SendDNS();
+            if (dnssuccess)
+            {
+                return "Success: Ping Sent";
+            }
+            else
+            {
+                return "Failed: Ping Not Sent";
+            }
+        }
+    }
+}
---
+++ TestProject/PingTest/NetworkUtilityTest.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp.Ping;
+using FakeItEasy;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.DNS;
+
+namespace TestProject.PingTest
+{
+    public class NetworkUtilityTest
+    {
+        private readonly IDNS _dNS;
+        private readonly NetworkUtility _pingService;
+        public NetworkUtilityTest()
+        {
+            _dNS= A.Fake<IDNS>();
+            _pingService = new NetworkUtility(_dNS);
+        }
+        [Fact]
+        public void NetworkUtility_SendPing_ReturnsString()
+        {
+            //Arrange
+            A.CallTo(() => _dNS.SendDNS()).Returns(true);
+            //Act
+            var result= _pingService.SendPing();
+            //Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Success: Ping Sent");
+            result.Should().Contain("Success", Exactly.Once());
+        }
+    }   
+}
